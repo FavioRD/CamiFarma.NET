9a31627e1432548588ce076869f1e7f143aef3e5
Favio Ramos <I202402947@cibertec.edu.pe>
2025-08-08 19:24:09 -0500

    Implementar CRUD de productos en la aplicación
    Se han añadido funcionalidades para crear, editar y eliminar productos en los archivos `ProductoController.cs` y `ProductoService.cs`. Se han actualizado las vistas `Crear.cshtml`, `Editar.cshtml`, `Eliminar.cshtml` e `Index.cshtml` para permitir la interacción del usuario. Además, se han corregido detalles en la conexión a la base de datos y se han utilizado procedimientos almacenados para las operaciones de base de datos.

--- CamiFarma_I/Controllers/ProductoController.cs
+++ CamiFarma_I/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
-﻿using CamiFarma_I.Services;
+﻿using CamiFarma_I.Models;
+using CamiFarma_I.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CamiFarma_I.Controllers
@@ -17,6 +18,69 @@ namespace CamiFarma_I.Controllers
         {
             var productos = _productoService.ObtenerTodos();
             return View(productos);
+        }
+
+        // GET: /Producto/Crear
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: /Producto/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _productoService.Insertar(producto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: /Producto/Editar/5
+        public IActionResult Editar(int id)
+        {
+            var producto = _productoService.ObtenerPorId(id); 
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: /Producto/Editar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _productoService.Editar(producto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: /Producto/Eliminar/5
+        public IActionResult Eliminar(int id)
+        {
+            var producto = _productoService.ObtenerPorId(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: /Producto/EliminarConfirmado/5
+        [HttpPost, ActionName("EliminarConfirmado")]
+        [ValidateAntiForgeryToken]
+        public IActionResult EliminarConfirmado(int id)
+        {
+            _productoService.Eliminar(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- CamiFarma_I/Services/ProductoService.cs
+++ CamiFarma_I/Services/ProductoService.cs
@@ -1,5 +1,5 @@
 ﻿using System.Data;
-using Microsoft.Data.SqlClient; 
+using Microsoft.Data.SqlClient;
 using CamiFarma_I.Models;
 
 namespace CamiFarma_I.Services
@@ -23,36 +23,120 @@ namespace CamiFarma_I.Services
             _connectionString = $"Server={sqlServer};Database=FarmaciaDB;User Id={sqlUser};Password={sqlPass};Encrypt=False;TrustServerCertificate=True;";
         }
 
+        // Listar
         public List<Producto> ObtenerTodos()
         {
             var productos = new List<Producto>();
 
             using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("USP_ListarProductos", connection))
             {
-                using (var command = new SqlCommand("USP_ListarProductos", connection))
-                {
-                    command.CommandType = CommandType.StoredProcedure;
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
 
-                    connection.Open();
-                    using (var reader = command.ExecuteReader())
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        productos.Add(new Producto
                         {
-                            productos.Add(new Producto
-                            {
-                                Id = Convert.ToInt32(reader["Id"]),
-                                Nombre = reader["Nombre"].ToString(),
-                                Descripcion = reader["Descripcion"].ToString(),
-                                Precio = Convert.ToDecimal(reader["Precio"]),
-                                Stock = Convert.ToInt32(reader["Stock"]),
-                                FechaExpiracion = Convert.ToDateTime(reader["FechaExpiracion"])
-                            });
-                        }
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Nombre = reader["Nombre"].ToString(),
+                            Descripcion = reader["Descripcion"].ToString(),
+                            Precio = Convert.ToDecimal(reader["Precio"]),
+                            Stock = Convert.ToInt32(reader["Stock"]),
+                            FechaExpiracion = Convert.ToDateTime(reader["FechaExpiracion"])
+                        });
                     }
                 }
             }
 
             return productos;
         }
+
+        // Agregar producto
+        public void Insertar(Producto producto)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("USP_InsertarProducto", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Nombre", producto.Nombre);
+                    command.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+                    command.Parameters.AddWithValue("@Precio", producto.Precio);
+                    command.Parameters.AddWithValue("@Stock", producto.Stock);
+                    command.Parameters.AddWithValue("@FechaExpiracion", producto.FechaExpiracion);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Editar producto
+        public void Editar(Producto producto)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("USP_EditarProducto", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Id", producto.Id);
+                command.Parameters.AddWithValue("@Nombre", producto.Nombre);
+                command.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+                command.Parameters.AddWithValue("@Precio", producto.Precio);
+                command.Parameters.AddWithValue("@Stock", producto.Stock);
+                command.Parameters.AddWithValue("@FechaExpiracion", producto.FechaExpiracion);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Eliminar producto
+        public void Eliminar(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand("USP_EliminarProducto", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Id", id);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public Producto ObtenerPorId(int id)
+        {
+            Producto producto = null;
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT * FROM Productos WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        producto = new Producto
+                        {
+                            Id = (int)reader["Id"],
+                            Nombre = reader["Nombre"].ToString(),
+                            Descripcion = reader["Descripcion"].ToString(),
+                            Precio = (decimal)reader["Precio"],
+                            Stock = (int)reader["Stock"],
+                            FechaExpiracion = (DateTime)reader["FechaExpiracion"]
+                        };
+                    }
+                }
+            }
+
+            return producto;
+        }
+
     }
 }
